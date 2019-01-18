30af099fbc0c45fbb30b32538ee78e5bdc70745a
Jan-Christopher Pien <j.pien@searchmetrics.com>
2019-01-18 17:32:40 +0100

    Include Tags in Response

--- build.sbt
+++ build.sbt
@@ -9,6 +9,18 @@ scalaVersion := "2.10.4"
 cancelable in Global := true
 fork in run := true
 
+mainClass in assembly := Some("ch.ethz.dalab.web2text.Main")
+
+assemblyMergeStrategy in assembly := {
+  case PathList("javax", "xml", xs @ _*)         => MergeStrategy.first
+  case PathList("org", "apache", xs @ _*)         => MergeStrategy.first
+  case PathList("com", "esotericsoftware", xs @ _*)         => MergeStrategy.first
+  case PathList("com", "google", xs @ _*)         => MergeStrategy.first
+  case x =>
+    val oldStrategy = (assemblyMergeStrategy in assembly).value
+    oldStrategy(x)
+}
+
 libraryDependencies += "org.scalatest" % "scalatest_2.10" % "2.2.4" % "test"
 
 libraryDependencies += "org.apache.spark" %% "spark-core" % "1.4.1"
@@ -16,3 +28,5 @@ libraryDependencies += "org.apache.spark" %% "spark-core" % "1.4.1"
 libraryDependencies += "org.apache.spark" %% "spark-mllib" % "1.4.1"
 
 libraryDependencies += "org.mongodb" %% "casbah" % "3.1.0"
+
+libraryDependencies += "io.spray" %% "spray-json" % "1.3.5"
--- src/main/scala/ch/ethz/dalab/web2text/Main.scala
+++ src/main/scala/ch/ethz/dalab/web2text/Main.scala
@@ -1,41 +1,98 @@
 package ch.ethz.dalab.web2text
 
-import ch.ethz.dalab.web2text.utilities.Util
-import ch.ethz.dalab.web2text.utilities.Util._
-import ch.ethz.dalab.web2text.cleaneval.{CleanEval,Page}
-import ch.ethz.dalab.web2text.cdom.{CDOM,DOM}
-import org.jsoup.Jsoup
+import java.io.{BufferedWriter, File, FileWriter}
+
+import ch.ethz.dalab.web2text.cdom.CDOM
+import ch.ethz.dalab.web2text.cleaneval.CleanEval
+import ch.ethz.dalab.web2text.features.FeatureExtractor
 import ch.ethz.dalab.web2text.features.extractor._
-import ch.ethz.dalab.web2text.classification.{PerformanceStatistics}
-import ch.ethz.dalab.web2text.features.{BlockFeatureExtractor,FeatureExtractor}
-import ch.ethz.dalab.web2text.features.PageFeatures
-import com.mongodb.casbah.Imports._
-import java.io.File;
 import ch.ethz.dalab.web2text.output.CsvDatasetWriter
-import ch.ethz.dalab.web2text.utilities.Warc
-import ch.ethz.dalab.web2text.output.CleanTextOutput
-import scala.util.{Try,Success,Failure}
+import ch.ethz.dalab.web2text.utilities.Util
+import spray.json._
+
+import scala.io.Source
+//import DefaultJsonProtocol._
+
+// https://github.com/spray/spray-json#providing-jsonformats-for-case-classes
+case class MyOutput(blockFeatures: Array[Double], edgeFeatures: Array[Double], texts: Array[String], tags: Array[String])
+
+object MyOutput
+
+object MyJsonProtocol extends DefaultJsonProtocol {
+  implicit val myOutputFormat = jsonFormat4(MyOutput.apply)
+}
+
+import ch.ethz.dalab.web2text.MyJsonProtocol._
+
 
 object Main {
 
   def main(args: Array[String]): Unit = {
-    // testWarcLoad
-    // exportFeaturesTest
-    exportFeaturesTest
+    val text = getFromStdin()
+    //    val text = getFromFile("/Users/onurkuru/work/web2textFEATURES/data/bbc-news-page.html")
+
+    val jsonOutput = getJsonOutput(text)
+    //    writeToFile("data/json_test.json", jsonOutput)
+    println(jsonOutput)
+  }
+
+  def getFromStdin(): String = {
+    val lines = Source.stdin.getLines()
+    val text = lines.mkString("\n")
+    return text
+  }
+
+  def getFromFile(html_file_path: String): String = {
+    val bufferedSource = Source.fromFile(html_file_path)
+    val fileContents = bufferedSource.getLines.mkString
+    bufferedSource.close
+    return fileContents
+  }
+
+  def writeToFile(filePath: String, text: String) = {
+    val file = new File(filePath)
+    val bw = new BufferedWriter(new FileWriter(file))
+    bw.write(text)
+    bw.close()
+  }
+
+  def getJsonOutput(html_text: String): String = {
+    val dom = CDOM.fromHTML(html_text)
+    val fe = FeatureExtractor(
+      DuplicateCountsExtractor
+        + LeafBlockExtractor
+        + AncestorExtractor(NodeBlockExtractor + TagExtractor(mode = "node"), 1)
+        + AncestorExtractor(NodeBlockExtractor, 2)
+        + RootExtractor(NodeBlockExtractor)
+        + TagExtractor(mode = "leaf"),
+      TreeDistanceExtractor + BlockBreakExtractor + CommonAncestorExtractor(NodeBlockExtractor)
+    )
+    val leaf_texts = new Array[String](dom.leaves.length)
+    val leaf_types = new Array[String](dom.leaves.length)
+    for (i <- 0 to (leaf_texts.length - 1)) {
+      leaf_texts(i) = dom.leaves(i).text
+      leaf_types(i) = dom.leaves(i).tags.mkString(" ")
+    }
+    val features = fe(dom)
+    val myOutput = new MyOutput(features.blockFeatures.data, features.edgeFeatures.data, leaf_texts, leaf_types)
+    return myOutput.toJson.toString()
   }
 
   def exportFeaturesTest = {
     val fe = FeatureExtractor(
       DuplicateCountsExtractor
-      + LeafBlockExtractor
-      + AncestorExtractor(NodeBlockExtractor + TagExtractor(mode="node"), 1)
-      + AncestorExtractor(NodeBlockExtractor, 2)
-      + RootExtractor(NodeBlockExtractor)
-      + TagExtractor(mode="leaf"),
+        + LeafBlockExtractor
+        + AncestorExtractor(NodeBlockExtractor + TagExtractor(mode = "node"), 1)
+        + AncestorExtractor(NodeBlockExtractor, 2)
+        + RootExtractor(NodeBlockExtractor)
+        + TagExtractor(mode = "leaf"),
       TreeDistanceExtractor + BlockBreakExtractor + CommonAncestorExtractor(NodeBlockExtractor)
     )
-    val data = Util.time{ CleanEval.dataset(fe) }
-    CsvDatasetWriter.write(data, "/Users/tvogels/Desktop/export")
+    val data = Util.time {
+      CleanEval.dataset(fe)
+    }
+    //    CsvDatasetWriter.write(data, "/Users/thijs/Desktop/export")
+    CsvDatasetWriter.write(data, "/Users/onurkuru/work/web2textFEATURES/export")
     println("# Block features")
     fe.blockExtractor.labels.foreach(println)
     println("# Edge features")
@@ -45,23 +102,23 @@ object Main {
   def testCommonAncestorExtractor = {
     val ex = CommonAncestorExtractor(LeafBlockExtractor)
     val cdom = CDOM.fromHTML("<body><h1>Header</h1><p>Paragraph with an <i>Italic</i> section.</p></body>");
-    ex(cdom)(cdom.leaves(2),cdom.leaves(1))
+    ex(cdom)(cdom.leaves(2), cdom.leaves(1))
   }
 
   def evaluateOtherMethods = {
     val dir = "other_frameworks/output/"
     val cleaners = Iterable(
-      "victor"            -> ((id: Int) => s"$dir/victor/$id-aligned.txt"),
-      "bte"               -> ((id: Int) => s"$dir/bte/$id-aligned.txt"),
+      "victor" -> ((id: Int) => s"$dir/victor/$id-aligned.txt"),
+      "bte" -> ((id: Int) => s"$dir/bte/$id-aligned.txt"),
       "article-extractor" -> ((id: Int) => s"$dir/article-extractor/$id-aligned.txt"),
       "default-extractor" -> ((id: Int) => s"$dir/default-extractor/$id-aligned.txt"),
-      "largest-content"   -> ((id: Int) => s"$dir/largestcontent-extractor/$id-aligned.txt"),
-      "unfluff"           -> ((id: Int) => s"$dir/unfluff/$id-aligned.txt")
+      "largest-content" -> ((id: Int) => s"$dir/largestcontent-extractor/$id-aligned.txt"),
+      "unfluff" -> ((id: Int) => s"$dir/unfluff/$id-aligned.txt")
     )
 
     for ((label, filenameGen) <- cleaners) {
       val title = s"#### Evaluating ‘${label.capitalize}’ "
-      println(s"\n$title${"#"*(82-title.length)}\n")
+      println(s"\n$title${"#" * (82 - title.length)}\n")
       Util.time {
         val eval = CleanEval.evaluateCleaner(filenameGen)
         println(s"$eval")
@@ -70,7 +127,7 @@ object Main {
   }
 
   def alignCleanEvalData = {
-    val projectPath = "/Users/thijs/dev/boilerplate"
+    val projectPath = "/Users/onurkuru/work/web2textFEATURES"
     val dir = s"$projectPath/src/main/resources/cleaneval/aligned"
     CleanEval.generateAlignedFiles(dir)
   }
